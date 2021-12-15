4e96f6db90ae2e3feb8719584b6a4b57b4dc733b
Anuj <anujrai04082002.0@gmail.com>
2021-12-15 19:26:18 +0530

    Created the Second Layout Login Layout status:Successfully

---
+++ app/src/main/java/com/example/offiqlresturantapp/ui/login/LoginScreenFragment.kt
@@ -0,0 +1,17 @@
+package com.example.offiqlresturantapp.ui.login
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.offiqlresturantapp.R
+import com.example.offiqlresturantapp.databinding.LoginScreenFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginScreenFragment : Fragment(R.layout.login_screen_fragment) {
+    private lateinit var binding: LoginScreenFragmentBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = LoginScreenFragmentBinding.bind(view)
+    }
+}
--- app/src/main/java/com/example/offiqlresturantapp/ui/testingconnection/TestingConnectionFragment.kt
+++ app/src/main/java/com/example/offiqlresturantapp/ui/testingconnection/TestingConnectionFragment.kt
@@ -1,8 +1,9 @@
-package com.example.offiqlresturantapp.ui
+package com.example.offiqlresturantapp.ui.testingconnection
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.offiqlresturantapp.R
 import com.example.offiqlresturantapp.databinding.TestingConnectionFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,10 @@ class TestingConnectionFragment : Fragment(R.layout.testing_connection_fragment)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = TestingConnectionFragmentBinding.bind(view)
-
+        binding.testConnectionId.setOnClickListener {
+            val action =
+                TestingConnectionFragmentDirections.actionTestingConnectionFragmentToLoginScreenFragment()
+            findNavController().navigate(action)
+        }
     }
 }
