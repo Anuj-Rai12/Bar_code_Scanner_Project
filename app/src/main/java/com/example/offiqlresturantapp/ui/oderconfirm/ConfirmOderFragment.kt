e5e1a71d18c813930657897001b5d37e083bd2bd
Anuj <anujrai04082002.0@gmail.com>
2021-12-23 15:55:46 +0530

    Added the Swipe Layout at the place of Deleted Button status:successfully

--- app/src/main/java/com/example/offiqlresturantapp/ui/oderconfirm/ConfirmOderFragment.kt
+++ app/src/main/java/com/example/offiqlresturantapp/ui/oderconfirm/ConfirmOderFragment.kt
@@ -1,26 +1,33 @@
 package com.example.offiqlresturantapp.ui.oderconfirm
 
 import android.annotation.SuppressLint
+import android.graphics.Canvas
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.offiqlresturantapp.R
 import com.example.offiqlresturantapp.databinding.ConfirmOrderLayoutBinding
 import com.example.offiqlresturantapp.ui.oderconfirm.adaptor.ConfirmOderFragmentAdaptor
 import com.example.offiqlresturantapp.ui.oderconfirm.model.FoodItemSelected
 import com.example.offiqlresturantapp.utils.*
 import dagger.hilt.android.AndroidEntryPoint
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 
 @AndroidEntryPoint
 class ConfirmOderFragment : Fragment(R.layout.confirm_order_layout) {
     private lateinit var binding: ConfirmOrderLayoutBinding
     private lateinit var confirmOderFragmentAdaptor: ConfirmOderFragmentAdaptor
-
+    private lateinit var list: MutableList<FoodItemSelected>
+    private lateinit var callback: ItemTouchHelper.SimpleCallback
 
     @SuppressLint("SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.M)
@@ -33,6 +40,7 @@ class ConfirmOderFragment : Fragment(R.layout.confirm_order_layout) {
         }
         setRecycle()
         setData()
+        setCallBack()
         binding.viewOfferBtn.setOnClickListener {
             binding.orderRecycleViewHint.hide()
             binding.listOfItemRecycleView.show()
@@ -52,7 +60,7 @@ class ConfirmOderFragment : Fragment(R.layout.confirm_order_layout) {
     }
 
     private fun setData() {
-        val list = listOf(
+        list = mutableListOf(
             FoodItemSelected(
                 foodName = "Chloe Bhature",
                 foodAmt = 125,
@@ -86,6 +94,91 @@ class ConfirmOderFragment : Fragment(R.layout.confirm_order_layout) {
 
         confirmOderFragmentAdaptor.submitList(list)
     }
+
+    private fun setCallBack() {
+        callback = object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ) = false
+
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val getItem =
+                    confirmOderFragmentAdaptor.currentList[viewHolder.absoluteAdapterPosition]
+                getItem?.let {
+                    list.remove(it)
+                    confirmOderFragmentAdaptor.submitList(list)
+                    confirmOderFragmentAdaptor.notifyDataSetChanged()
+                }
+            }
+
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+
+                RecyclerViewSwipeDecorator.Builder(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+                    .addSwipeLeftBackgroundColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.color_red
+                        )
+                    )
+                    .addSwipeLeftLabel("Delete")
+                    .setSwipeLeftLabelColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.white
+                        )
+                    )
+                    .setSwipeLeftLabelTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
+                    .addSwipeLeftActionIcon(R.drawable.ic_round_delete)
+                    .setSwipeLeftActionIconTint(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.white
+                        )
+                    )
+                    .create()
+                    .decorate()
+
+
+
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+            }
+
+        }
+
+        val itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(binding.listOfItemRecycleView)
+    }
+
 
     private fun setRecycle() {
         binding.listOfItemRecycleView.apply {
