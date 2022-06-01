6a693905824eaf8423bb75148057e3d265bd7309
Anuj <anujrai04082002.0@gmail.com>
2022-06-02 01:32:56 +0530

    Added the Item click list

--- app/src/main/java/com/example/motionlyt/MainActivity.kt
+++ app/src/main/java/com/example/motionlyt/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
     }
 
     override fun onBackPressed() {
