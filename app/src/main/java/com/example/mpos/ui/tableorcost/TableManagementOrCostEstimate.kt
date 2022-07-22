fab0ef56bc12fa69ba869593b4c4bdf2abe69942
Anuj <anujrai04082002.0@gmail.com>
2022-07-23 00:32:05 +0530

    Added Table Reservation Screen status:Success

--- app/src/main/java/com/example/mpos/ui/tableorcost/TableManagementOrCostEstimate.kt
+++ app/src/main/java/com/example/mpos/ui/tableorcost/TableManagementOrCostEstimate.kt
@@ -1,19 +1,19 @@
 package com.example.mpos.ui.tableorcost
 
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mpos.R
 import com.example.mpos.databinding.TableOrCostLayoutBinding
 import com.example.mpos.ui.tableorcost.adaptor.TableManagementOrCostRecyclerAdaptor
 import com.example.mpos.ui.tableorcost.model.SelectionDataClass
-import com.example.mpos.utils.*
+import com.example.mpos.utils.TAG
+import com.example.mpos.utils.changeStatusBarColor
+import com.example.mpos.utils.showDialogBox
 
 
 class TableManagementOrCostEstimate : Fragment(R.layout.table_or_cost_layout) {
@@ -22,14 +22,13 @@ class TableManagementOrCostEstimate : Fragment(R.layout.table_or_cost_layout) {
     private lateinit var tableManagementOrCostRecyclerAdaptor: TableManagementOrCostRecyclerAdaptor
     private val args: TableManagementOrCostEstimateArgs by navArgs()
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.changeStatusBarColor()
         binding = TableOrCostLayoutBinding.bind(view)
         binding.mposId3.text = args.storeName
         setRecycleView()
-        setData()
+        tableManagementOrCostRecyclerAdaptor.submitList(SelectionDataClass.list)
         //removeItemFromBackStack()
         //showCountOfBackStack()
         binding.logoutBtnIc2.setOnClickListener {
@@ -58,33 +57,32 @@ class TableManagementOrCostEstimate : Fragment(R.layout.table_or_cost_layout) {
             setHasFixedSize(true)
             tableManagementOrCostRecyclerAdaptor = TableManagementOrCostRecyclerAdaptor {
                 Log.i(TAG, "setRecycleView: $it")
-                val action = if (it.title == getString(R.string.item_table)) {
-                    TableManagementOrCostEstimateDirections.actionTableManagementOrCostEstimateToTableManagementFragment(
-                        args.storeName
-                    )
-                } else {
-                    TableManagementOrCostEstimateDirections.actionTableManagementOrCostEstimateToCostDashBoardFragment(
-                        null,
-                        null
-                    )
-                }
-                findNavController().navigate(action)
+                screenNav(it)
             }
             adapter = tableManagementOrCostRecyclerAdaptor
         }
     }
 
-    private fun setData() {
-        val list = listOf(
-            SelectionDataClass(
-                image = R.drawable.ic_waiter,
-                title = getString(R.string.item_table)
-            ),
-            SelectionDataClass(
-                image = R.drawable.ic_cost_estimation,
-                title = getString(R.string.item_cost)
-            )
-        )
-        tableManagementOrCostRecyclerAdaptor.submitList(list)
+    private fun screenNav(selection: SelectionDataClass) {
+        when (SelectionDataClass.Companion.RestaurantSelection.valueOf(selection.type)) {
+            SelectionDataClass.Companion.RestaurantSelection.TABLE_MANAGEMENT -> {
+                val action =
+                    TableManagementOrCostEstimateDirections
+                        .actionTableManagementOrCostEstimateToTableManagementFragment(args.storeName)
+                findNavController().navigate(action)
+            }
+            SelectionDataClass.Companion.RestaurantSelection.COST_ESTIMATION -> {
+                val action =
+                    TableManagementOrCostEstimateDirections.actionTableManagementOrCostEstimateToCostDashBoardFragment(
+                        null,
+                        null
+                    )
+                findNavController().navigate(action)
+            }
+            SelectionDataClass.Companion.RestaurantSelection.TABLE_RESERVATION -> {
+
+            }
+        }
     }
+
 }
--- app/src/main/java/com/example/mpos/ui/tableorcost/model/SelectionDataClass.kt
+++ app/src/main/java/com/example/mpos/ui/tableorcost/model/SelectionDataClass.kt
@@ -1,7 +1,37 @@
 package com.example.mpos.ui.tableorcost.model
 
+import com.example.mpos.R
+
 //Sample Model Class
 data class SelectionDataClass(
     val image: Int,
-    val title: String
-)
+    val title: String,
+    val type: String
+) {
+    companion object {
+        val list = listOf(
+            SelectionDataClass(
+                image = R.drawable.ic_waiter,
+                title = "Table Management",
+                type = RestaurantSelection.TABLE_MANAGEMENT.name
+            ),
+            SelectionDataClass(
+                image = R.drawable.ic_table_dinner,
+                title = "Table Reservation",
+                type = RestaurantSelection.TABLE_RESERVATION.name
+            ),
+            SelectionDataClass(
+                image = R.drawable.ic_cost_estimation,
+                title = "Cost Estimate",
+                type = RestaurantSelection.COST_ESTIMATION.name
+            )
+        )
+
+        enum class RestaurantSelection {
+            TABLE_MANAGEMENT,
+            COST_ESTIMATION,
+            TABLE_RESERVATION
+        }
+
+    }
+}
