83b9827d5055224f0d523f410d8a0119864fe564
Anuj <anujrai04082002.0@gmail.com>
2022-09-22 14:53:53 +0530

    Removed some bug status:Success

--- app/src/main/java/com/example/mpos/ui/restaturantbilling/RestaurantBillingFragment.kt
+++ app/src/main/java/com/example/mpos/ui/restaturantbilling/RestaurantBillingFragment.kt
@@ -9,6 +9,7 @@ import com.example.mpos.utils.changeStatusBarColor
 
 class RestaurantBillingFragment : Fragment(R.layout.restaurant_billing_fragment) {
     private lateinit var binding: RestaurantBillingFragmentBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requireActivity().changeStatusBarColor(R.color.semi_white_color_two)
